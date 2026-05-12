a55fd4564ed0d60dfcef30e72c7563e5b526e2df
Sophia Campana <78273225+sophmcamp@users.noreply.github.com>
2026-05-12 00:41:41 -0400

    Refactor output handling in PassengerPigeonPoem
    Refactored the main method to consolidate output handling and removed redundant print statements.

--- src/PassengerPigeonPoem.java
+++ src/PassengerPigeonPoem.java
@@ -1,31 +1,37 @@
 /**
- * Author: Sophia Campana (Alexandria, Virginia)
- * Date: April 11, 2018
+ * Author: Sophia Campana
+ * Original date: April 11, 2018
  *
  * The Poem in the Pigeons by Emily Suvada (from This Mortal Coil) 
  *
  * This program will convert the DNA base pairs to binary, then to ASCII,
- * and then to alphanumeric letters to decyrpt the poem.
+ * and then to alphanumeric letters to decode the poem.
  *
  */
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class PassengerPigeonPoem {
     public static void main(String[] args) {
         
        PassengerPigeonInputter input = new PassengerPigeonInputter("src/PassengerPigeonPoemText.txt");
        
-       String poem = input.getDNAPoem();
-       System.out.println("Base pairs: " + poem);
+       try {
+           PrintStream out = new PrintStream("./DecodedPoem.txt");
+           System.setOut(out);
+           System.setErr(out);
+           System.out.print(
+               "Base pairs:\n" + input.getDNAPoem()
+               + "\n\nBinary:\n" + input.getbinPoem()
+               + "\n\nBinary separated by 8 digits:\n" + input.get8binPoem()
+               + "\n\nASCII:\n" + input.getASCII()
+               + "\n\nAlphanumeric:\n" + input.getText()
+               + "\n\nAlphanumeric:\n" + input.getText()
+               );
+       }
+       catch (FileNotFoundException e) {
+           e.printStackTrace();
+       }
        
-       poem = input.getbinPoem();
-       System.out.println("Binary:     " + poem);  
-       
-       poem = input.get8binPoem();
-       System.out.println("Binary separated by 8 digits: " + poem);
-      
-       poem = input.getASCII();
-       System.out.println("ASCII: " + poem);
-       
-       poem = input.getText();
-       System.out.println("Alphanumeric:\n\n" + poem);
     }
  }
